c3a913bdbfcf7f6e99200aba7403005ecb0ee972
Andrei Simionescu <andrei@fmi-online.com>
2015-01-28 15:36:41 +0100

    Parses email and pushes to db

--- README.md
+++ README.md
@@ -1,5 +1,33 @@
-## Lavaboom mailer
+Lavaboom mailer
+---------------
 
 Lavaboom's SMTP gateway to the world wide web.
 
 Currently only receives emails. Soon outbound email support, via postfix, will be added.
+
+How it works
+------------
+
+`mailer` is a Dockerized app that has 3 components:
+
+-	Haraka + our plugins -> receives emails
+-	Postfix in send-only mode -> sends emails
+-	Spamassassin daemon
+
+### Receiving emails
+
+1.	check domain is in the `mx_hosts` table
+2.	check the email address
+	1.	`@lavaboom.tld`? check account name
+	2.	`@custom.domain`? check `accounts` table for email address
+3.	parse email
+	1.	create IDs for attachments
+	2.	create document in `emails` table
+	3.	upload attachments to `attachments`
+4.	post NATS message to topic `received`
+
+### Sending emails
+
+1.	receive NATS message on `send`
+2.	read email from database, format it
+3.	send it to Postfix and post NATS message `delivered` when finished
--- plugins/queue.rethinkdb.js
+++ plugins/queue.rethinkdb.js
@@ -3,23 +3,47 @@ var r = require("rethinkdb");
 exports.hook_queue = function(next, connection) {
 	var plugin = this;
 	var rdb = server.notes.rdb;
-	var dbName = rdb.config.db;
+	var transaction = connection.transaction;
+
 	plugin.loginfo("Received email! Saving to db.");
 
-	var transaction = connection.transaction;
-	var receivedDate = transaction.header.headers.data;
-	var subjectLine = transaction.header.headers.subject;
+	// rcpt.address_exists saves this to connection.notes
+	var rcptUsers = ["john's ID"];
 
-	r.db(dbName).table('emails').insert({
-		email: transaction.mail_from,
-		body: transaction.data_lines,
-		received: receivedDate || (new Date()),
-		subject: subjectLine,
-	}).run(rdb.conn, function(err, result) {
-		if (err) {
-			plugin.logwarn("Couldn't write email to database.");
-			return next(DENY, "Couldn't connect to Haraka");
-		}
-		return next();
-	});
+	var receivedDate = transaction.header.headers.data || (new Date());
+	var from = "" + transaction.mail_from.user + "@" + transaction.mail_from.host;
+	var to = [];
+	var subjectLine = transaction.header.get("subject").replace("\n", "");
+	var body = transaction.body.bodytext || "<empty>";
+
+	plugin.loginfo(transaction.header.toString());
+
+	for (var i in transaction.rcpt_to) {
+		to.push(transaction.rcpt_to[i].format());
+	}
+
+	for (var i in rcptUsers) {
+		r.table('emails').insert({
+			id: server.notes.uuid(),
+			date_created: receivedDate,
+			date_modified: receivedDate,
+			name: 'email',
+			owner: rcptUsers[i],
+			from: from,
+			to: to,
+			cc: [],
+			bcc: [],
+			attachments: [], // TODO add list of attachment IDs
+			body: body,
+			status: "received",
+			subject: subjectLine,
+			is_read: false,
+		}).run(rdb.conn, function(err, result) {
+			if (err) {
+				plugin.logcrit("Couldn't write email to database.");
+				return next(DENY, "Couldn't write to RethinkDB");
+			}
+			return next();
+		});
+	}
 }
