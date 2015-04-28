6eb24bc7269b03170b4a01d453c67f284d3a83a2
Piotr Zduniak <piotr@zduniak.net>
2015-04-28 23:03:01 +0200

    startup.sh

---
+++ _postfix/startup.sh
@@ -0,0 +1,7 @@
+echo ">> starting postfix"
+service postfix start
+
+echo ">> printing the logs"
+touch /var/log/mail.log /var/log/mail.err /var/log/mail.warn
+chmod a+rw /var/log/mail.*
+tail -F /var/log/mail.*
